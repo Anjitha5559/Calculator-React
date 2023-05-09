568c039f6dad9f663a70e3f0e4ac18fdede9297b
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2023-05-09 20:59:06 +0530

    Anjitha

--- calculator@react/src/App.jsx
+++ calculator@react/src/App.jsx
@@ -30,17 +30,17 @@ const calculate = () =>{
       <button onClick={clear} id="clear"className="span-two">AC</button>
       <button onClick={backspace} id="backspace" >DEL</button>
       <button onClick={handleClick} name="/">&divide;</button>
-      <button onClick={handleClick} name="1">1</button>
-      <button onClick={handleClick} name="2"> 2</button>
-      <button onClick={handleClick} name="3">3</button>
+      <button onClick={handleClick} name="7">7</button>
+      <button onClick={handleClick} name="8">8</button>
+      <button onClick={handleClick} name="9">9</button>
       <button onClick={handleClick} name="*">&times;</button>
       <button onClick={handleClick} name="4">4</button>
       <button onClick={handleClick} name="5">5</button>
       <button onClick={handleClick} name="6">6</button>
       <button onClick={handleClick} name="+">+</button>
-      <button onClick={handleClick} name="7">7</button>
-      <button onClick={handleClick} name="8">8</button>
-      <button onClick={handleClick} name="9">9</button>
+      <button onClick={handleClick} name="1">1</button>
+      <button onClick={handleClick} name="2">2</button>
+      <button onClick={handleClick} name="3">3</button>
       <button onClick={handleClick} name="-">&ndash;</button>
       <button onClick={handleClick} name=".">.</button>
       <button onClick={handleClick} name="0">0</button>
